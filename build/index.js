a9395b79788613643c61d5136861bf3b365e29f8
Sohail Shrestha <sohailshrestha2@gmail.com>
2024-05-20 12:54:46 +0545

    fixed issues with git ignore

--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const printer_1 = require("./submodule/printer");
-(0, printer_1.printer)();
--- build/submodule/printer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.printer = void 0;
-const printer = () => {
-    console.log("Hello world");
-};
-exports.printer = printer;
